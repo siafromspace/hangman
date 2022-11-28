348e9e2b227abd488b0edd7723868fced4fae424
siafromspace <sharonyak15@gmail.com>
2022-11-28 02:22:09 +0100

    started work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import FlashcardList from "./components/FlashcardList";
+import axios from "axios";
 
 function App() {
+  const [flashcards, setFlashcards] = useState([])
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+         .then(res => {
+          setFlashcards(res.data.results.map((questionItem, index) => {
+            const answer = questionItem.answer
+            const options = [...questionItem.incorrect_answers, answer]
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: questionItem.correct_answer,
+              options: options.sort(() => Math.random() - 0.5)
+            }
+          }))
+         })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FlashcardList flashcards={flashcards} />
     </div>
-  );
+  )
+}
+
+const decodeString = (str) => {
+  const textArea = document.createElement('textarea')
+  textArea.innerHTML = str
+  return textArea.value
 }
 
 export default App;
---
+++ src/components/FlashcardList.jsx
@@ -0,0 +1,13 @@
+import Flashcard from "./Flashcard";
+
+const FlashcardList = ({flashcards}) => {
+    return ( 
+        <div className="card-grid">
+            {flashcards.map(flashcard => {
+                return <Flashcard flashcard={flashcard} key={flashcard.id} />
+            })}
+        </div>
+     );
+}
+ 
+export default FlashcardList;
